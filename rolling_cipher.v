62f0515b34e29f09ac3c78bd6536e00628284d68
thatcrappyguy <aldrineve69@gmail.com>
2022-06-15 13:39:23 +0530

    Added function to decrypt a cipher text given the roll integer

--- rolling_cipher.v
+++ rolling_cipher.v
@@ -7,7 +7,17 @@ fn return_cipher_text(text string, roll_q int) string{
 	return rune_arr.string()
 }
 
+fn decrypt(cipher_text string, roll_q int) string{
+	mut rune_arr := cipher_text.runes()
+	a := rune_arr.len 
+	for i in 0..a{
+		rune_arr[i] -= roll_q 
+	}
+	return rune_arr.string()
+}
+
 fn main(){
 	secret := "Don't go there tomorrow!"
 	println(return_cipher_text(secret, 5))
+	println(decrypt(return_cipher_text(secret, 5), 5))
 }
