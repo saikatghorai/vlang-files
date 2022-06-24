3a6cef3171076da5440b6216ce895443468a6e2c
thatcrappyguy <aldrineve69@gmail.com>
2022-06-24 16:12:06 +0530

    Added a preliminary implementation of a brainfuck compiler in V

--- bf.v
+++ bf.v
@@ -10,28 +10,28 @@ fn parse(text string) {
 	mut cellrow := []int{len: cell_row_size, init: 0}
 	for i in text {
 		match i.ascii_str() {
-			">" {
+			'>' {
 				if idx == 1000 {
 					idx = 0
 				} else {
 					idx++
 				}
 			}
-			"<" {
+			'<' {
 				if idx == 0 {
 					idx = 1000
 				} else {
 					idx++
 				}
 			}
-			"+" {
+			'+' {
 				if cellrow[idx] > data_limit {
 					cellrow[idx] %= data_limit
 				} else {
 					cellrow[idx]++
 				}
 			}
-			"-" {
+			'-' {
 				if cellrow[idx] < 0 {
 					cellrow[idx] %= data_limit
 					cellrow[idx]++
@@ -39,15 +39,13 @@ fn parse(text string) {
 					cellrow[idx]--
 				}
 			}
-			"." {
+			'.' {
 				println(rune(cellrow[idx]))
 			}
-			"," {
+			',' {
 				cellrow[idx] = os.input('').int()
 			}
-			else {
-				
-			}
+			else {}
 		}
 	}
 }
